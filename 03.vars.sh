45118ef00b8ef988cca1a2781c0b87ed9b936254
rbeera <rbeera79@gmail.com>
2021-06-08 15:18:13 +1000

    Adding new files 2

---
+++ 03.vars.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## Declare a variable
+
+CourseName=DevOps
+
+## Access the variable
+echo Course Name = $CourseName
+
+# DATE=2021-06-08
+DATE=$(date +%F)
+echo Good Morning, Todayd date is $DATE
